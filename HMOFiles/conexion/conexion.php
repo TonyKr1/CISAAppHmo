33aa336830089fccdf772182e33d8cc8ec6c5a26
Desarrollo-TI <TonyKr1>
2023-09-24 22:28:46 -0600

    add servidor HMO

--- HMOFiles/conexion/conexion.php
+++ HMOFiles/conexion/conexion.php
@@ -1,6 +1,6 @@
 <?php
-$serverName = "192.168.30.5"; //serverName\instanceName
-$connectionInfo = array( "Database"=>"CISAWEBAPPSHMODEV", "UID"=>"sa", "PWD"=>"a1b2c3d4*1","CharacterSet" => "UTF-8");
+$serverName = "192.168.150.8"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"CISAWEBAPPSHMOQA", "UID"=>"sa", "PWD"=>"System123","CharacterSet" => "UTF-8");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
 if( !$conn ) {
