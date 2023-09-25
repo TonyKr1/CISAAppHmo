cfbdee6d85ed807a62a08844550d5a2634ccb629
Jose Antonio Cruz Romero <115579440+TonyKr1@users.noreply.github.com>
2023-09-25 11:45:07 -0600

    Delete HMOFiles/conexion directory

--- HMOFiles/conexion/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$serverName = "192.168.150.8"; //serverName\instanceName
-$connectionInfo = array( "Database"=>"CISAWEBAPPSHMOQA", "UID"=>"sa", "PWD"=>"System123","CharacterSet" => "UTF-8");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
-
-if( !$conn ) {
-     die( print_r( sqlsrv_errors(), true));
-}
-?>
